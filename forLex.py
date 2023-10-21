1aa99ca16f9df3919ea3eaa54f0fd70fc7322596
siddhi-zanwar <147896015+siddhi-zanwar@users.noreply.github.com>
2023-10-21 16:40:15 +0530

    Add files via upload

---
+++ forLex.py
@@ -0,0 +1,23 @@
+import ply.lex as lex
+tokens = ('FOR', 'VAR', 'IN', 'DO', 'DONE',
+          'VALUE', 'COMMAND')
+
+t_FOR = r'for'
+t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_IN = r'in'
+t_DO = r'do'
+
+def t_VALUE(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_COMMAND(t):
+    r'\$[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+def t_error(t):
+    print(f"Illegal character: {t.value[0]}")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
