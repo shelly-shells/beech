cf7b0d35e3f908f558fe2b5d5f9d81fa1bc61b08
siddhi-zanwar <pes1202201256@pesu.pes.edu>
2023-10-23 11:12:16 +0530

    blah

--- forLex.py
+++ forLex.py
@@ -17,7 +17,7 @@ def t_COMMAND(t):
     return t
 
 def t_error(t):
-    print(f"Illegal character: {t.value[0]}")
+    print("Illegal character: {t.value[0]}")
     t.lexer.skip(1)
 
 lexer = lex.lex()
